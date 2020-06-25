0ff732b6ec9130ad1870220f473130c23ab9ef4b
caiocroci <caiocroci@gmail.com>
2020-06-25 06:21:06 -0300

    Salvar o tt

---
+++ Assets/Scripts/Disable.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disable : MonoBehaviour
+{
+	public GameObject boost1;
+	public GameObject boost4;
+
+
+	private void OnTriggerEnter(Collider collision)
+	{
+		if (collision.CompareTag("Player"))
+		{
+			Destroy(boost1);
+			boost4.SetActive(true);
+
+		}
+	}
+}
